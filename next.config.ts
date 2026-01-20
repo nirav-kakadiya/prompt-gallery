c76b7928e8cf37815ed8430611a8a228c009795b
nirav-kakadiya <nirav@infinitycorp.tech>
2026-01-20 11:57:37 +0530

    // Allow images from any HTTPS source

--- next.config.ts
+++ next.config.ts
@@ -2,61 +2,57 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // Allow images from any HTTPS source
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "blogimages.smartshot.ai",
-        pathname: "/**",
-      },
-      // Twitter/X images
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        pathname: "/**",
-      },
-      // Reddit images
-      {
-        protocol: "https",
-        hostname: "i.redd.it",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "preview.redd.it",
-        pathname: "/**",
-      },
-      // Imgur (commonly used on Reddit)
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        pathname: "/**",
-      },
-      // CloudFront CDN for user-uploaded images
-      {
-        protocol: "https",
-        hostname: "*.cloudfront.net",
-        pathname: "/**",
-      },
-      // PromptHero CDN
-      {
-        protocol: "https",
-        hostname: "cdn.prompthero.com",
-        pathname: "/**",
+      //   hostname: "images.unsplash.com",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "*.googleusercontent.com",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "avatars.githubusercontent.com",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "blogimages.smartshot.ai",
+      //   pathname: "/**",
+      // },
+      // // Twitter/X images
+      // {
+      //   protocol: "https",
+      //   hostname: "pbs.twimg.com",
+      //   pathname: "/**",
+      // },
+      // // Reddit images
+      // {
+      //   protocol: "https",
+      //   hostname: "i.redd.it",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "preview.redd.it",
+      //   pathname: "/**",
+      // },
+      // // Imgur (commonly used on Reddit)
+      // {
+      //   protocol: "https",
+      //   hostname: "i.imgur.com",
+      //   pathname: "/**",
+      // },
+      // // CloudFront CDN for user-uploaded images
+      // {
+      //   protocol: "https",
+      //   hostname: "*.cloudfront.net",
+      //   pathname: "/**",
+        hostname: "**",
       },
     ],
   },
